4e4f318809b65161caa63aa0a8e37e5fbd397d38
AnvarZIyodov <ziyodovanvar1999@gmail.com>
2025-04-21 22:53:25 +0900

    Feat: develop convex and clerk authentication

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+
+export default {
+    providers: [
+        {
+            domain: 'https://driving-honeybee-36.clerk.accounts.dev',
+            applicationID: "convex",
+        },
+    ]
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,19 @@ export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignUpButton />
+        <SignUpButton >
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ">
+            Sign Up
+          </button>
+        </SignUpButton>
       </SignedOut>
 
       <SignedIn>
-        <SignedOut />
+        <SignedOut >
+        <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ">
+            Sign In
+          </button>
+        </SignedOut>
       </SignedIn>
     </div>
 
