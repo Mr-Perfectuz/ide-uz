7ea747d08b76466babac410bd7ef16330e7989f4
AnvarZiyodov <ziyodovanvar1999@gmail.com>
2025-04-22 14:56:10 +0900

    Feat: develop Schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -11,4 +11,35 @@ export default defineSchema({
     lemonSqueezyCustomerId: v.optional(v.string()),
     lemonSqueezyOrderId: v.optional(v.string()),
   }).index("by_userId", ["userId"]),
+
+  codeExecutions: defineTable({
+    userId: v.string(),
+    language: v.string(),
+    code: v.string(),
+    output: v.optional(v.string()),
+    error: v.optional(v.string()),
+  }).index("by_userId", ["userId"]),
+
+  snippets: defineTable({
+    userId: v.string(),
+    title: v.string(),
+    language: v.string(),
+    code: v.string(),
+    userName: v.string(),
+  }).index("by_user_id", ["userId"]),
+
+  snippetComments: defineTable({
+    snippetId: v.id("snippets"),
+    userId: v.string(),
+    userName: v.string(),
+    content: v.string(),
+  }).index("by_snippet_id", ["snippetId"]),
+
+  stars: defineTable({
+    userId: v.string(),
+    snippetId: v.id("snippets"),
+  })
+    .index("by_user_id", ["userId"])
+    .index("by_snippet_id", ["snippetId"])
+    .index("by_user_id_and_snippet_id", ["userId", "snippetId"]),
 });
