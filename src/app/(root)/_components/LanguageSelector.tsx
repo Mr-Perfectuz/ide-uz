fbb77899e94de0e2fce125d1fb091b696022f085
AnvarZiyodov <ziyodovanvar1999@gmail.com>
2025-04-22 18:10:54 +0900

    Feat: develop LoginButton

---
+++ src/app/(root)/_components/LanguageSelector.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
+
+function LanguageSelector({ hasAccess }: { hasAccess: boolean }) {
+  return <div></div>;
+}
+
+export default LanguageSelector;
