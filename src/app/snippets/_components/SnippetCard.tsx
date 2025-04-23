0a8cfb80cc18d1b13b001000ca53666959df994c
AnvarZiyodov <ziyodovanvar1999@gmail.com>
2025-04-23 11:24:22 +0900

    Feat: develop SnippetCard SnippetsPageSkeleton SnippetsPage NavigationHeader components

---
+++ src/app/snippets/_components/SnippetCard.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Snippet } from "@/types";
+import React from "react";
+
+const SnippetCard = ({ snippet }: { snippet: Snippet }) => {
+  return <div>SnippetCard</div>;
+};
+
+export default SnippetCard;
---
+++ src/app/snippets/_components/SnippetsPageSkeleton.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SnippetsPageSkeleton = () => {
+  return <div>SnippetsPageSkeleton</div>;
+};
+
+export default SnippetsPageSkeleton;
